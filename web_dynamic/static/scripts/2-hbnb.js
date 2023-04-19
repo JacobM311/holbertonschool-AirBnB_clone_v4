a830643ba33ce19100afbc7603a5d32e7824cf63
JasonBeagle <skipskipperson385@gmail.com>
2023-04-19 10:40:20 -0700

    task3 and 5

---
+++ web_dynamic/static/scripts/2-hbnb.js
@@ -0,0 +1,23 @@
+$(function () {
+    const amenityIds = {};
+  
+    $('.amenities input[type=checkbox]').change(function () {
+      if (this.checked) {
+        amenityIds[$(this).data('name')] = $(this).data('id');
+      } else {
+        delete amenityIds[$(this).data('name')];
+      }
+  
+      $('.amenities h4').text(Object.keys(amenityIds).join(', '));
+    });
+  
+    $.get('http://0.0.0.0:5001/api/v1/status/', function (data, textStatus) {
+      if (textStatus === 'success') {
+        if (data.status === 'OK') {
+          $('div#api_status').addClass('available');
+        } else {
+          $('div#api_status').removeClass('available');
+        }
+      }
+    });
+});
